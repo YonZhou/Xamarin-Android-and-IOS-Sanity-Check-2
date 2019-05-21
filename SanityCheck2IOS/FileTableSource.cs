ba67e18c95d842e634809d0315d333f52dc9677f
YonZhou <yonzhouschool@gmail.com>
2019-05-21 16:31:28 -0700

    added IOS Implementation, just started on getting the file listing

---
+++ SanityCheck2IOS/FileTableSource.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace SanityCheck2IOS
+{
+    public class FileTableSource : UITableViewSource
+    {
+        string[] FileNames;
+        //string[] ImageURLs;
+        string CellIdentifier = "fileTableCell";
+        public FileTableSource(string[] fileNames)
+        {
+            this.FileNames = fileNames;
+        }
+
+        public static UIImage FromUrl(string uri)
+        {
+            using (var url = new NSUrl(uri))
+            using (var data = NSData.FromUrl(url))
+                return UIImage.LoadFromData(data);
+        }
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            UITableViewCell cell = tableView.DequeueReusableCell(CellIdentifier);
+            string name = FileNames[indexPath.Row];
+
+            //---- if there are no cells to reuse, create a new one
+            if (cell == null)
+            {
+                cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier);
+                cell.ImageView.Image = FromUrl("http://LAPTOP-EJBJ9OK5:8080/A17_FlightPlan.png");
+            }
+
+            cell.TextLabel.Text = name;
+
+            return cell;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return FileNames.Length;
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            UIAlertController okAlertController = UIAlertController.Create("Row Selected", FileNames[indexPath.Row], UIAlertControllerStyle.Alert);
+            okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+
+	        tableView.DeselectRow(indexPath, true);
+        }
+    }
+}
---
+++ SanityCheck2IOS/ViewController.cs
@@ -0,0 +1,34 @@
+﻿using Foundation;
+using System;
+using System.Net;
+using UIKit;
+
+namespace SanityCheck2IOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            var table = new UITableView(View.Bounds); // defaults to Plain style
+
+            WebClient client = new WebClient();
+
+            string[] tableItems = new string[] { "A17_FlightPlan" };
+            table.Source = new FileTableSource(tableItems);
+            Add(table);
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
